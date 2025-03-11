fbd69562cf15d40f869d6202fc3c394fb7a59075
djordan015 <69061900+djordan015@users.noreply.github.com>
2025-03-11 14:51:29 -0700

    finish cosine similarity and being building engine

--- cosine_similarity.py
+++ cosine_similarity.py
@@ -1,14 +1,19 @@
-from sklearn.feature_extraction.text import CountVectorizer
-import numpy as np
+# test file to help understand how 
+# cosine_similarity works 
 
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 
 text = ["London Paris London", "Paris Paris London"]
 vectorizer = CountVectorizer()
-X = vectorizer.fit_transform(text)
+x = vectorizer.fit_transform(text)
 
 [[2,1], [1, 2]]
 
 print("Vocabulary:", vectorizer.vocabulary_)
 print("Feature Names:", vectorizer.get_feature_names_out())
-print("Bag of Words Representation:\n", X.toarray())
+print("Bag of Words Representation:\n", x.toarray())
+
+print("cosine_similarity")
+print(cosine_similarity(x))
